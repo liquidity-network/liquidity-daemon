e8d2b1e7b1fdd603a26d78d0ea271023ffd90997
george <kajogo777@gmail.com>
2018-10-30 16:02:53 +0200

    Modify existing endpoints to follow the REST convention

--- daemon/src/routes/wallet.ts
+++ daemon/src/routes/wallet.ts
@@ -1,7 +1,7 @@
 import Wallet from "../controllers/wallet";
 
 export function route(app) {
-    app.route('/wallet/information')
+    app.route('/wallet')
         .get(async (request, response) => {
             response.send(await Wallet.information())
         })
